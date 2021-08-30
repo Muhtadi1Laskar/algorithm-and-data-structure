8d7590f39d7f1a898a4e87eb7bdc70c26bd13df3
Muhtadi <muhtadi@kinetik.care>
2021-08-30 19:33:40 +0600

    Added Depth First Search

--- Data Structures/BinarySearchTree.js
+++ Data Structures/BinarySearchTree.js
@@ -66,6 +66,48 @@ class BinarySearchTree {
       }
     }
   }
+  DFSInOrder() {
+    return traverseInOrder(this.root, []);
+  }
+  DFSPostOrder() {
+    return traversePostOrder(this.root, []);
+  }
+  DFSPreOrder() {
+    return traversePreOrder(this.root, []);
+  }
+}
+
+const traverseInOrder = (node, list) => {
+  if(node.left) {
+    traverseInOrder(node.left, list);
+  }
+  list.push(node.value);
+  if(node.right) {
+    traverseInOrder(node.right, list);
+  }
+  return list;
+}
+
+const traversePreOrder = (node, list) => {
+  list.push(node.value);
+  if(node.left) {
+    traversePreOrder(node.left, list);
+  }
+  if(node.right) {
+    traversePreOrder(node.right, list);
+  }
+  return list;
+}
+
+const traversePostOrder = (node, list) => {
+  if(node.left) {
+    traversePostOrder(node.left, list);
+  }
+  if(node.right) {
+    traversePostOrder(node.right, list);
+  }
+  list.push(node.value);
+  return list;
 }
 
 const tree = new BinarySearchTree();
