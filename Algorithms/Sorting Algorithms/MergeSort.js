a27e6834c64df00bcfda507b3b6d8cd9a7d572f0
Muhtadi <muhtadi@kinetik.care>
2021-11-15 17:35:48 +0600

    Modified mergesort code

--- Algorithms/Sorting Algorithms/MergeSort.js
+++ Algorithms/Sorting Algorithms/MergeSort.js
@@ -1,19 +1,21 @@
 const merge = (left, right) => {
-  let array = [];
-
-  while (left.length && right.length) {
-    left[0] < right[0] ? array.push(left.shift()) : array.push(right.shift());
+  let i = 0;
+  let j = 0;
+  let li=[];
+  while(i < left.length && j < right.length) {
+    left[i] < right[j] ? li.push(left[i++]) : li.push(right[j++]);
   }
-  return [...array, ...left, ...right];
-};
+  return [...li, ...left.slice(i, left.lenght), ...right.slice(j, right.lenght)];
+}
 
 const mergeSort = (array) => {
-  const half = Math.floor(array.length / 2);
-
-  if (array.length < 2) {
+  if(array.length === 1) {
     return array;
   }
+  let middleIndex = Math.floor(array.length/2);
+  let leftArray = array.slice(0, middleIndex);
+  let rightArray = array.slice(middleIndex, array.length);
+  return merge(mergeSort(leftArray), mergeSort(rightArray));
+}
 
-  const left = array.splice(0, half);
-  return merge(mergeSort(left), mergeSort(array));
-};
+console.log(mergeSort([4, 3, 7, 2, 1, 6, 5, 9, 8]));
