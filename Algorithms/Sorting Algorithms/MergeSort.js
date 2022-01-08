c8a00c49ef5d3219b7c89e4bdf1a4e5b4833daf9
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2022-01-09 03:44:54 +0600

    Added functional approach of merge sort

--- Algorithms/Sorting Algorithms/MergeSort.js
+++ Algorithms/Sorting Algorithms/MergeSort.js
@@ -19,3 +19,29 @@ const mergeSort = (array) => {
 }
 
 console.log(mergeSort([4, 3, 7, 2, 1, 6, 5, 9, 8]));
+
+
+// Functional Approach
+const mergeF = (a, b) => {
+  if (a.length === 0) {
+    return b;
+  } else if (b.length === 0) {
+    return a;
+  } else if (a[0] < b[0]) {
+    return [a[0], ...mergeF(a.slice(1), b)];
+  } else {
+    return [b[0], ...mergeF(a, b.slice(1))];
+  }
+};
+
+const mergeSortF = (array) => {
+  if (array.length < 2) {
+    return array;
+  }
+  let middleIdx = Math.floor(array.length / 2);
+  let left = array.slice(0, middleIdx);
+  let right = array.slice(middleIdx);
+  return mergeF(mergeSortF(left), mergeSortF(right));
+};
+
+console.log(mergeSortF([5, 4, 3, 2, 1]));
